517cafa1ad5a850f05e8f1158be90f3ca289950f
designravenous <32999911+designravenous@users.noreply.github.com>
2018-06-30 14:11:52 +0200

    Update main.js
    - added xmlhttpRequest()
    still need to fix:
    - add all array results of JSON resonse
    - error handling on readystats & status

--- main.js
+++ main.js
@@ -1,12 +1,44 @@
 
+//  http://www.omdbapi.com/?apikey=xxxxxxxx&s=s%C3%B6karna&r=json
 
+var API = "?apikey=xxxxxxxx";
 var title;
 var year;
 var iD;
 var type;
 var poster;
+var url;
 
-function updateMovie(movie){
+function updateOfURL(film){
+    url = "http://www.omdbapi.com/" + API + "&s=" + film + "&r=json";
+    console.log(url);
+    sendRequester(url);
+}
+
+function sendRequester(url){
+    var xmlReq = new XMLHttpRequest();
+    console.log("hello");
+  xmlReq.onreadystatechange = function(){
+      if (xmlReq.readyState == 4 && xmlReq.status == 200){
+          var datas = JSON.parse(xmlReq.responseText);
+          var movie = {};
+          movie.title = datas.Search[0].Title;
+          movie.year = datas.Search[0].Year;
+          movie.iD = datas.Search[0].imdbID;
+          movie.type = datas.Search[0].Type;
+          movie.poster = datas.Search[0].Poster;
+          console.log(movie.poster);
+      }
+      updater(movie);
+  }
+        xmlReq.open("GET", url, true);
+        xmlReq.send();
+        
+        
+    }
+
+
+function updater(movie){
     title.innerHTML = movie.title; 
     year.innerHTML = movie.year;
     iD.innerHTML = movie.iD;
@@ -21,13 +53,8 @@ window.onload = function(){
     iD = document.getElementById("identitet");
     type = document.getElementById("typeOf");
     poster = document.getElementById("image");
-    
-    var movie={};
-    movie.title = "Sökarna";
-    movie.year = "19999";
-    movie.iD = "ID121212";
-    movie.type = "movie";
-    movie.poster ="https://images-na.ssl-images-amazon.com/images/M/MV5BMjE3NzY0NzMwM15BMl5BanBnXkFtZTcwMjkzMjA0MQ@@._V1_SX300.jpg"
 
-    updateMovie(movie);
+    var film = "Sökarna";
+    console.log(film);
+    updateOfURL(film);
 }
