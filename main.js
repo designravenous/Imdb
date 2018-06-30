f426374b3a3a2b75c704439e95f0c1c404855396
designravenous <32999911+designravenous@users.noreply.github.com>
2018-07-01 00:15:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,16 +8,20 @@ var iD;
 var type;
 var poster;
 var url;
+var errormsg;
+var titleOne;
+var yearOne;
+var iDOne;
+var typeOne;
+var posterOne;
 
 function updateOfURL(film){
     url = "http://www.omdbapi.com/" + API + "&s=" + film + "&r=json";
-    console.log(url);
     sendRequester(url);
 }
 
 function sendRequester(url){
     var xmlReq = new XMLHttpRequest();
-    console.log("hello");
   xmlReq.onreadystatechange = function(){
       if (xmlReq.readyState == 4 && xmlReq.status == 200){
           var datas = JSON.parse(xmlReq.responseText);
@@ -27,16 +31,37 @@ function sendRequester(url){
           movie.iD = datas.Search[0].imdbID;
           movie.type = datas.Search[0].Type;
           movie.poster = datas.Search[0].Poster;
-          console.log(movie.poster);
+          movie.titleOne = datas.Search[1].Title;
+          movie.yearOne = datas.Search[1].Year;
+          movie.iDOne = datas.Search[1].imdbID;
+          movie.typeOne = datas.Search[1].Type;
+          movie.posterOne =datas.Search[1].Poster;
+          errormsg.innerHTML = " "; 
+          var resp = datas.Response;
+
+          if (datas.Search[0].Poster == "N/A"){
+            movie.poster= "http://designravenous.com/no-image-found.jpg";
+        }
+          else if (datas.Search[1].Poster == "N/A"){
+            movie.posterOne= "http://designravenous.com/no-image-found.jpg";
+          }
+         
+          console.log(resp);
+          updater(movie);
+          console.log(url);  
+          
       }
-      updater(movie);
+      
   }
         xmlReq.open("GET", url, true);
         xmlReq.send();
-        
-        
     }
 
+    function onTheClick(){
+        var input = document.getElementById("input");
+        film = input.value;
+        updateOfURL(film);
+    }
 
 function updater(movie){
     title.innerHTML = movie.title; 
@@ -44,6 +69,11 @@ function updater(movie){
     iD.innerHTML = movie.iD;
     type.innerHTML = movie.type;
     poster.src = movie.poster;
+    titleOne.innerHTML = movie.titleOne;
+    yearOne.innerHTML = movie.yearOne;
+    typeOne.innerHTML = movie.typeOne;
+    posterOne.src = movie.posterOne;
+    iDOne.innerHTML =movie.iDOne;
 }
 
 
@@ -53,8 +83,14 @@ window.onload = function(){
     iD = document.getElementById("identitet");
     type = document.getElementById("typeOf");
     poster = document.getElementById("image");
+    errormsg = document.getElementById("errormsg");
+    titleOne = document.getElementById("titleOne");
+    yearOne = document.getElementById("yearOne");
+    iDOne = document.getElementById("identitetOne");
+    typeOne = document.getElementById("typeOfOne");
+    posterOne = document.getElementById("imageOne");
 
-    var film = "Sökarna";
+    var film = "star wars";
     console.log(film);
     updateOfURL(film);
 }
